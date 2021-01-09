e8215d8791eb87f9aecc551724cec0c2e7569aef
NazarDunets <jcq.nazardunets@gmail.com>
2021-01-09 10:51:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/basicviews_dunets_l13/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.basicviews_dunets_l13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
